8a8abc17b17a12eacc68a8f365ac8859c5f43e82
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-11-27 12:27:42 -0300

    Finalizado a atividade do dia 27/11

---
+++ src/main/java/com/generation/blogpessoal/model/Postagens.java
@@ -0,0 +1,28 @@
+package com.generation.blogpessoal.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_postagens")
+@Getter
+@Setter
+public class Postagens {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Size(min = 5, max = 100)
+    private String titulo;
+    @NotBlank
+    @Size(min = 5, max = 1000)
+    private String texto;
+    @UpdateTimestamp
+    private LocalDate data;
+}
