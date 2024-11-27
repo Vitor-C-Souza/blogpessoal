7becad1c37ae2a6f8c8b907e4c98a18c281336bd
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-11-27 12:27:42 -0300

    Finalizado a atividade do dia 27/11

---
+++ src/main/java/com/generation/blogpessoal/PostagemController.java
@@ -0,0 +1,25 @@
+package com.generation.blogpessoal;
+
+import com.generation.blogpessoal.model.Postagens;
+import com.generation.blogpessoal.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+    @Autowired
+    private PostagemRepository postagemRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Postagens>> getAll() {
+        return ResponseEntity.ok(postagemRepository.findAll());
+    }
+}
