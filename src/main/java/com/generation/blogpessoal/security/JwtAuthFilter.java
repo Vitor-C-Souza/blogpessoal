199a23d2d8ff5f13a994bb1f05f02ecc500b4e49
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-12-06 12:27:37 -0300

    finalizado o sistema de security

--- src/main/java/com/generation/blogpessoal/security/JwtAuthFilter.java
+++ src/main/java/com/generation/blogpessoal/security/JwtAuthFilter.java
@@ -13,11 +13,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Autowired
